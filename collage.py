1781017d6f26cf576bf20a6d82c5209795dceca7
Maor Kadosh <git@avocadosh.xyz>
2021-10-21 16:27:01 +0300

    eh just some comments in collage.py

--- collage.py
+++ collage.py
@@ -1,10 +1,10 @@
 import os
 from itertools import product
-from typing import List, Tuple
+from typing import List
 from urllib.request import urlopen
 
 import pylast
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from pylast import Album
 
 IMAGE_EDGE_SIZE = 300
@@ -13,50 +13,60 @@ FONT_SIZE = 15
 
 
 def fetch_albums() -> List[Album]:
-    network = pylast.LastFMNetwork(
-        api_key=os.getenv("LASTFM_API_KEY"), api_secret=os.getenv("LASTFM_API_SECRET")
-    )
-
+    """
+    Fetch my 9 most listened albums from the past month
+    Expects LASTFM_API_KEY and LASTFM_API_SECRET env vars
+    """
+    api_key = os.getenv("LASTFM_API_KEY")
+    api_secret = os.getenv("LASTFM_API_SECRET")
+    network = pylast.LastFMNetwork(api_key, api_secret)
     items = network.get_user("jpegaga").get_top_albums(pylast.PERIOD_1MONTH, limit=9)
     return [item.item for item in items]
 
 
 def fetch_cover(album: Album) -> Image.Image:
+    "Download the album's cover image and return a Pillow Image of it"
     return Image.open(urlopen(album.get_cover_image())).convert("RGBA")
 
 
-def text_overlay(text: str, base_size: Tuple[int, int]) -> Image.Image:
+def overlay_text(album: Album) -> str:
+    "Returns the string to be written on the album overlay"
+    artist = album.artist.name if album.artist and album.artist.name else ""
+    return f"{album.title or ''}\n{artist}"
+
+
+def overlay(text: str) -> Image.Image:
+    """
+    An image of some text in a cool font on top of a black rectangle stretching across the whole
+    image horizontally
+    Font location should be defined via COLLAGE_TTF env var
+    """
     fnt = ImageFont.truetype(os.getenv("COLLAGE_TTF"), FONT_SIZE)
     height = fnt.getsize_multiline(text)[1] + TEXT_BG_BOTTOM_PADDING
 
-    rect = Image.new("RGBA", base_size, (255, 255, 255, 0))
+    rect = Image.new("RGBA", (IMAGE_EDGE_SIZE, IMAGE_EDGE_SIZE), (255, 255, 255, 0))
 
     draw = ImageDraw.Draw(rect)
-    draw.rectangle((0, 0, base_size[0], height), fill=(0, 0, 0, 180))
+    draw.rectangle((0, 0, IMAGE_EDGE_SIZE, height), fill=(0, 0, 0, 180))
     draw.text((0, 0), text, font=fnt)
 
     return rect
 
 
-def art_with_text(album: Album):
-    artist = album.artist.name if album.artist and album.artist.name else ""
-    text = f"{album.title or ''}\n{artist}"
-
-    album_img = fetch_cover(album)
-    text_img = text_overlay(text, album_img.size)
-
-    return Image.alpha_composite(album_img, text_img)
-
-
 def generate_collage():
-    img = Image.new("RGBA", (3 * IMAGE_EDGE_SIZE, 3 * IMAGE_EDGE_SIZE))
+    result_img = Image.new("RGBA", (3 * IMAGE_EDGE_SIZE, 3 * IMAGE_EDGE_SIZE))
 
     print("Fetching albums...")
+
     # place covers in (0, 300), (0, 600), (0, 900), (300, 0) ...
     for album, (x, y) in zip(fetch_albums(), product(range(3), range(3))):
         print(album)
-        album_img = art_with_text(album)
-        img.paste(album_img, (x * IMAGE_EDGE_SIZE, y * IMAGE_EDGE_SIZE))
+
+        base_cover = fetch_cover(album)
+        album_label = overlay_text(album)
+        img = Image.alpha_composite(base_cover, overlay(album_label))
+
+        result_img.paste(img, (x * IMAGE_EDGE_SIZE, y * IMAGE_EDGE_SIZE))
 
     # from dust we came and to dust we will return
-    return img.convert("RGB")
+    return result_img.convert("RGB")
